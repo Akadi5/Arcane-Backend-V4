dba71c9191280abb0400b786fe75cc7a4de4a312
Akadi5 <105155313+Akadi5@users.noreply.github.com>
2024-12-04 11:01:27 +0100

    done

--- src/discord/commands/addvbucks.ts
+++ src/discord/commands/addvbucks.ts
@@ -2,6 +2,8 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { EmbedBuilder } from "discord.js";
 import Profile from '../../database/models/profiles';
 
+//ignore this for now. will finish later or i wont even work on this anymore
+
 module.exports = {
     data = new SlashCommandBuilder()
     .setName("addvbucks")
